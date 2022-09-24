1dadf02d6d489fac02d88637af267f81d9d8aa2a
MUTINDAH <petahmutinda@gmail.com>
2022-09-24 13:10:04 +0300

    modified 5-print_comb2.py file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for number in range(0, 100):
     if number < 99:
         print("{0:0=2d}, ".format(number), end="")
     if number == 99:
-        print("{0:0=2d}\n".format(number))
+        print("{0:0=2d}".format(number))
