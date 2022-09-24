659c1e255ce190568102ef7cd947f1f2ee510747
MUTINDAH <petahmutinda@gmail.com>
2022-09-24 15:12:17 +0300

    added 6-print_comb3.py file

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pytho3
+for first_num in range(0, 10):
+    for second_num in range(0, 10):
+        if first_num < second_num:
+            print("{}{}".format(first_num, second_num), end="")
+            if first_num < 8:
+                print(", ", end="")
+print("\n", end="")
