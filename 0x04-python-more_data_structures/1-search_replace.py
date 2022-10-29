d4ed1644b39b201f2b5a65b0cb2cbc36e028b1b3
MUTINDAH <petahmutinda@gmail.com>
2022-10-29 15:37:28 +0300

    modified 1-search_replace.py file

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,3 @@
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] in [search]:
-            my_list[i] = replace
-    return my_list
-    print(my_list)
+    return [replace if item == search else item for item in my_list]
