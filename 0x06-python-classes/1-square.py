80d6f5c1edc3fe776312f2aac8b47c665b334180
HealthTeam17 <recordhealthteam17@gmail.com>
2023-01-23 23:46:32 +0300

    Private instance attribute: size Instantiation with size (no type/value verification) You are not allowed to import any module

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Private instance attribute: size"""
+        self.__size = size
+
