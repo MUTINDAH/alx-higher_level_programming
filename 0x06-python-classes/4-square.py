6d2cd8fc06b6dc1d31eca80d03933859a7615e0d
HealthTeam17 <recordhealthteam17@gmail.com>
2023-01-24 21:50:57 +0300

    property def size(self): to retrieve it property setter def size(self, value): to set it: size must be an integer, otherwise raise a TypeError exception with the message size must be an integer if size is less than 0, raise a ValueError exception with the message size must be >= 0

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,15 +9,17 @@ class Square:
         """Private instance attribute: size"""
         self.__size = size
 
+    def area(self):
+        return self.__size * self.__size
+
+    @property
     def size(self):
         return self.__size
 
+    @size.setter
     def size(self, value):
         if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
         if self.__size < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    def area(self):
-        return self.__size * self.__size
