fbde6ce890385b5c4f665803d8c828cdae7c0846
HealthTeam17 <recordhealthteam17@gmail.com>
2023-01-28 11:41:08 +0300

    Write a function that divides 2 integers and prints the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    quotient = (a / b)
+    result = a / b
     try:
-        return quotient
+        result
     except ZeroDivisionError:
-        quotient = None
+        result = None
     finally:
-        print("Inside result: {:d}".format(quotient))
-    return quotient
+        print("Inside result: {}".format(result))
+    return result
