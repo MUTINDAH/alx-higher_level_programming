10ca61197577e48735b4fb019f5d0d893fa47bfe
HealthTeam17 <recordhealthteam17@gmail.com>
2023-01-28 09:55:59 +0300

    a function that prints x elements of a list.

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """The function will attempt to print x elements from the list, with each element separated by a space"""
+    print_elements = 0
+    try:
+        """if there are not enough elements in the list to print x elements, the function will print as many
+         elements as it can without raising an error."""
+        for i in range(x):
+            print(my_list[i], end=' ')
+            print_elements += 1
+    except IndexError:
+        pass
+    print()
+    """The function then returns the number of elements that were actually printed."""
+    return print_elements
