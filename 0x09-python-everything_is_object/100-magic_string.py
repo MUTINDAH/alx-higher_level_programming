492a6653f1e52f4de5ac4bf65c6219806ebab326
HealthTeam17 <recordhealthteam17@gmail.com>
2023-01-30 22:47:26 +0300

    Write a function magic_string() that returns a string “BestSchool” n times the number of the iteration (see code):
    Format: see example
    Your file should be maximum 4-line long (no documentation needed)
    You are not allowed to import any module

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# !/usr/bin/python3
 def magic_string():
     magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return "\n".join(", ".join(["BestSchool"] * i) for i in range(1, magic_string.n + 1)) + "$"
+    return "BestSchool, " * (magic_string.n - 1) + "BestSchool"
