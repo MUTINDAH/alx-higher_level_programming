3d33305be1566f3b9f2d10d87af831160b9e715c
HealthTeam17 <recordhealthteam17@gmail.com>
2023-04-13 21:43:46 +0300

    0x0B. Python - Input/Output files

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,77 +1,15 @@
 #!/usr/bin/python3
-"""
-Module defines a rectangle
-"""
+"""Defines a file-appending function."""
 
 
-class Rectangle:
+def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
     """
-    Class represents a rectangle
-    """
-
-    def __init__(self, width=0, height=0):
-        """
-        Initializes a Rectangle instance.
-
-        Args:
-            width (int): The width of the rectangle (default: 0).
-            height (int): The height of the rectangle (default: 0).
-        """
-        self.width = width
-        self.height = height
-
-    @property
-    def width(self):
-        """Gets the width of the rectangle."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """
-        Sets the width of the rectangle.
-
-        Args:
-            value (int): The width of the rectangle.
-
-        Raises:
-            TypeError: if the value is not an integer.
-            ValueError: if the value is negative.
-        """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Gets the height of the rectangle"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """
-        Sets the height of the rectangle.
-
-        Args:
-            value (int): The height of the rectangle.
-
-        Raises:
-            TypeError: if height is not an integer.
-            ValueError: if height is negative
-        """
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        """Returns the area of the Rectangle"""
-        return self.__height * self.__width
-
-    def perimeter(self):
-        """Returns the perimeter of the Rectangle."""
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        return 2 * (self.__height + self.__width)
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
