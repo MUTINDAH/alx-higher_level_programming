d794fc87c52dcb26fad2d40f561b08367e9b622a
HealthTeam17 <recordhealthteam17@gmail.com>
2023-04-13 21:15:50 +0300

    0x0B. Python - Input/Output files

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -76,13 +76,3 @@ class Rectangle:
             return 0
         return 2 * (self.__height + self.__width)
 
-# my_rectangle = Rectangle(2, 4)
-# print("Area: {} - Perimeter: {}"
-#       .format(my_rectangle.area(), my_rectangle.perimeter()))
-
-# print("--")
-
-# my_rectangle.width = 10
-# my_rectangle.height = 3
-# print("Area: {} - Perimeter: {}"
-#       .format(my_rectangle.area(), my_rectangle.perimeter()))
