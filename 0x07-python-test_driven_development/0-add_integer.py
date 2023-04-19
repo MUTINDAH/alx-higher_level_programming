40aa6fcb8a6242472f1abbb5b11da9dc216c33bf
MUTINDAH <petahmutinda@gmail.com>
2023-04-19 16:54:49 +0300

    edited 0-add_integer.txt

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,40 +1,17 @@
 #!/usr/bin/python3
-"""
-Module: 0-add_integer
-Contains a function that returns an int sum of two ints/floats.
-"""
+"""Defines an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """
-    This function takes two numbers as input parameters and returns their sum.
-    Args:
-        a (int or float): The first parameter.
-        b (int or float, optional): The second parameter. Default is 98.
+    """Return the integer addition of a and b.
 
-    Returns:
-        int: sum of a and b.
+    Float arguments are typecasted to ints before addition is performed.
 
     Raises:
-        TypeError: If either of `a` or `b` is not an integer or a float.
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-
-    if type(a) is not int and type(a) is not float:
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    elif type(b) is not int and type(b) is not float:
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    else:
-        return int(a) + int(b)
-
-# print(add_integer(1, 2))
-# print(add_integer(100, -2))
-# print(add_integer(2))
-# print(add_integer(100.3, -2))
-# try:
-#     print(add_integer(4, "School"))
-# except Exception as e:
-#     print(e)
-# try:
-#     print(add_integer(None))
-# except Exception as e:
-#     print(e)
+    return int(a) + int(b)
